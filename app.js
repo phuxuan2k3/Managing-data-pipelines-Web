67e1485d723d5477599ee1a1024546b45722b70c
NguyenPhamPhuXuan <phuxuan.px2003@gmail.com>
2024-09-18 16:12:43 +0700

    save

--- app.js
+++ app.js
@@ -1,48 +1,59 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const ENV = process.env;
-const path = require("path");
-const configEV = require("./config/configEV");
-const configStaticResource = require("./config/configStaticResource");
-const configSession = require("./config/configSession");
-const { NotFound, HandleError } = require("./middlewares/ErrorHandling");
-const passport = require("./config/mainPassport");
-const bodyParser = require("body-parser");
-const flash = require("express-flash");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const sslServer = require("./config/configSSL");
+// const express = require("express");
+// const app = express();
+// require("dotenv").config();
+// const ENV = process.env;
+// const path = require("path");
+// const configEV = require("./config/configEV");
+// const configStaticResource = require("./config/configStaticResource");
+// const configSession = require("./config/configSession");
+// const { NotFound, HandleError } = require("./middlewares/ErrorHandling");
+// const passport = require("./config/mainPassport");
+// const bodyParser = require("body-parser");
+// const flash = require("express-flash");
+// const cors = require("cors");
+// const cookieParser = require("cookie-parser");
+// const sslServer = require("./config/configSSL");
 
-// Config
-app.use(express.urlencoded({ extended: true }));
-configEV(app, path.join(__dirname, "views"));
-configStaticResource(app, path.join(__dirname, "public"));
-configSession(app);
-app.use(bodyParser.json());
-app.use(flash());
-app.use(cors({ credentials: true, origin: true }));
-app.use(cookieParser("sgx"));
+// // Config
+// app.use(express.urlencoded({ extended: true }));
+// configEV(app, path.join(__dirname, "views"));
+// configStaticResource(app, path.join(__dirname, "public"));
+// configSession(app);
+// app.use(bodyParser.json());
+// app.use(flash());
+// app.use(cors({ credentials: true, origin: true }));
+// app.use(cookieParser("sgx"));
 
-//No Caching
-// app.use((req, res, next) => {
-//     res.header('Cache-Control', 'no-store, no-cache, must-revalidate');
-//     next();
-// });
+// //No Caching
+// // app.use((req, res, next) => {
+// //     res.header('Cache-Control', 'no-store, no-cache, must-revalidate');
+// //     next();
+// // });
 
-//No authentication
-app.use(passport.initialize());
-app.use(passport.session());
+// //No authentication
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-//Router
-app.use("/api", require("./routers/api.r"));
+// //Router
+// app.use("/api", require("./routers/api.r"));
 
-app.use("/", require("./routers/site.r"));
+// app.use("/", require("./routers/site.r"));
 
-//Handle error middleware
-app.use(NotFound);
-app.use(HandleError);
+// //Handle error middleware
+// app.use(NotFound);
+// app.use(HandleError);
 
-const appSSL = sslServer(app);
+// const appSSL = sslServer(app);
 
-appSSL.listen(ENV.WEBPORT);
+// appSSL.listen(ENV.WEBPORT);
+
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 8080;
+app.get("/", function (req, res) {
+  res.sendfile("index.html");
+});
+server.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
--- config/configEV.js
+++ config/configEV.js
@@ -1,10 +1,10 @@
-const { engine } = require('express-handlebars');
-const helpers = require('../utils/helpers');
+const { engine } = require("express-handlebars");
+const helpers = require("../utils/helpers");
 
-const configEV = (app,filePath) => {
-    app.engine('hbs', engine({extname: ".hbs", helpers: helpers}));
-    app.set('view engine', 'hbs');
-    app.set('views', filePath);
-}
+const configEV = (app, filePath) => {
+  app.engine("hbs", engine({ extname: ".hbs", helpers: helpers }));
+  app.set("view engine", "hbs");
+  app.set("views", filePath);
+};
 
 module.exports = configEV;
